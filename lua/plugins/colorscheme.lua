88399a509b17e34e1c8107bc7c292d50ab0e7f72
Nathanael <nathanaelluke@proton.me>
2026-04-06 21:22:30 -0400

    Fixed the broken thing

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,24 @@
 return {
   {
     "casedami/neomodern.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = {
-      bg = "default",
-      theme = "moon",
-      gutter = {
-        cursorline = false,
-        dark = false,
-      },
-      diagnostics = {
-        darker = true,
-        undercurl = true,
-        background = true,
-      },
-      overrides = {},
-    },
-    config = function(_, opts)
-      require("neomodern").setup(opts)
-    end,
-  },
-  {
-    "vague-theme/vague.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
-    config = function(_, opts)
-      require("vague").setup(opts)
+    config = function()
+      require("neomodern").setup({
+        bg = "default",
+        theme = "moon",
+        gutter = {
+          cursorline = false,
+          dark = false,
+        },
+        diagnostics = {
+          darker = true,
+          undercurl = true,
+          background = true,
+        },
+        overrides = {},
+      })
+      require("neomodern").load()
     end,
   },
 }
