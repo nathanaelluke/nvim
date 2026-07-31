03050353ea33759b0d289f9f60ff299e60d5c2a7
Nathanael <nathanaelluke@proton.me>
2026-07-30 23:06:37 -0400

    Fix theme persistance for real this time

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
+
+local theme_file = vim.fn.stdpath("data") .. "/current_theme.txt"
+local f = io.open(theme_file, "r")
+local theme_to_load = "neomodern"
+
+if f then
+  local saved_theme = f:read("*l")
+  f:close()
+  if saved_theme and saved_theme ~= "" then
+    theme_to_load = saved_theme
+  end
+end
+
+pcall(vim.cmd.colorscheme, theme_to_load)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -50,19 +50,7 @@ local function quickfix()
     })
 end
 
-local theme_file = vim.fn.stdpath("data") .. "/current_theme.txt"
-local f = io.open(theme_file, "r")
-local theme_to_load = "neomodern"
 
-if f then
-  local saved_theme = f:read("*l")
-  f:close()
-  if saved_theme and saved_theme ~= "" then
-    theme_to_load = saved_theme
-  end
-end
-
-pcall(vim.cmd.colorscheme, theme_to_load)
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = { "*.cpp", "*.hpp", "*.c", "*.h" },
